856722102ff2cc16150a48e746cf803ceba0589f
DeanT99 <saucebossssss@gmail.com>
2020-10-24 17:01:07 -0500

    room and join pages

--- src/www/assets/js/scrollify/main.js
+++ src/www/assets/js/scrollify/main.js
@@ -3,17 +3,23 @@ $(function(){
     section: ".panel",
     scrollSpeed: 2000,
     offset : 0,
-    scrollbars: true,
-
-    before: function(section){
-      if(section===1){
-        $(".section2ColumnLeft").addClass("fadeInLeftBig");
-        $(".section2ColumnRight").addClass("fadeInRightBig");
-      
-
-      }
-
-    }
+    scrollbars: false,
   });
 
 });
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
